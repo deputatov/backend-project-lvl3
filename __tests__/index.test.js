3f7ec9c5cd641e2181fdf5ec93bb84104b5c4258
Deputatov Nikita <deputatovn@yandex.ru>
2020-06-12 16:50:24 +0300

    fix test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,19 +1,14 @@
 import nock from 'nock';
-import mock from 'mock-fs';
+import { promises as fs } from 'fs';
 import pageLoader from '../src/index.js';
 
-beforeEach(() => {
-  mock({
-    '/testpath': {},
-  });
-});
+const fixture = '__tests__/__fixtures__/test.html';
 
 test('page loader', async () => {
+  const data = await fs.readFile(fixture);
   const scope = nock('https://ru.hexlet.io')
     .get('/courses')
-    .reply(200);
-  await pageLoader('/testpath', 'https://ru.hexlet.io/courses');
+    .reply(200, data);
+  await pageLoader('/tmp', 'https://ru.hexlet.io/courses');
   expect(scope.isDone()).toBe(true);
 });
-
-afterEach(mock.restore);
