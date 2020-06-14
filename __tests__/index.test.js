ae3ab3ee170d8851b582b463f1a19726c55e086a
Deputatov Nikita <deputatovn@yandex.ru>
2020-06-14 12:44:34 +0300

    add beforeEach

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,14 +1,22 @@
+import fs from 'fs/promises';
+import os from 'os';
 import nock from 'nock';
-import { promises as fs } from 'fs';
+import path from 'path';
 import pageLoader from '../src/index.js';
 
 const fixture = '__tests__/__fixtures__/test.html';
+
+let testpath;
+
+beforeEach(async () => {
+  testpath = await fs.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
+});
 
 test('page loader', async () => {
   const data = await fs.readFile(fixture);
   const scope = nock('https://ru.hexlet.io')
     .get('/courses')
     .reply(200, data);
-  await pageLoader('/tmp', 'https://ru.hexlet.io/courses');
+  await pageLoader(testpath, 'https://ru.hexlet.io/courses');
   expect(scope.isDone()).toBe(true);
 });
